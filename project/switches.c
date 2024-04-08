1ed7bd342bfbbe11115be78236cbfaefe79b3d47
Luis Gomez <luisg31209@gmail.com>
2024-04-07 20:28:55 -0600

    added switches.c

--- project/switches.c
+++ project/switches.c
@@ -1,2 +1,23 @@
 #include <msp430.h>
 #include "led.h"
+#include "switches.h"
+
+void switch_init()
+{
+  P2REN |= SWITCHES;
+  P2IE |= SWITTCHES;
+  P2OUT |= SWITCHES;
+  P2DIR &= ~SWITCHES;
+}
+
+char switch_update_interrupt_sense()
+{
+  char p2val = P2IN;
+  P2IES |= (p2val & SWITCHES);
+  return p2val;
+}
+
+void switch_interrupt_handler()
+{
+    
+}

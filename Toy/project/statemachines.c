89153f91b07b89f577c6bbc3ce153f775bc034b0
Luis Gomez <luisg31209@gmail.com>
2024-04-13 23:03:10 -0600

    Final

--- Toy/project/statemachines.c
+++ Toy/project/statemachines.c
@@ -10,24 +10,28 @@ void next_state(int state) {
   switch(state) {
   case 1:
     led_flash(3);
+    leds_off();
     red_led_on();
     fur_Elise();
     leds_off();
     break;
   case 2:
     led_flash(3);
+    leds_off();
     red_led_on();
     happy_Bday();
     leds_off();
     break;
   case 3:
     led_flash(3);
+    leds_off();
     red_led_on();
     Mario();
     leds_off();
     break;
   case 4:
     led_flash(3);
+    leds_off();
     red_led_on();
     runaway();
     leds_off();
